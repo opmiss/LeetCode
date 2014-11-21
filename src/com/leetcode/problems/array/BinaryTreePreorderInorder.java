ad04924c42ecdb66d57c441f201ab8f356d272a8
opmiss <opmiss@gmail.com>
2014-11-20 21:40:08 -0800

    11202014

---
+++ src/com/leetcode/problems/array/BinaryTreePreorderInorder.java
@@ -0,0 +1,50 @@
+package com.leetcode.problems.array;
+
+import java.util.Stack;
+
+/* 
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+*/
+
+public class BinaryTreePreorderInorder {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		if (inorder.length == 0)
+			return null;
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		TreeNode root = new TreeNode(Integer.MIN_VALUE);
+		stack.push(root);
+		int i = 0, j = 0;
+		TreeNode node = null;
+		TreeNode cur = root;
+		while (j < inorder.length) {
+			if (stack.peek().val == inorder[j]) {
+				node = stack.pop();
+				j++;
+			} else if (node != null) {
+				cur = new TreeNode(preorder[i]);
+				node.right = cur;
+				node = null;
+				stack.push(cur);
+				i++;
+			} else {
+				cur = new TreeNode(preorder[i]);
+				stack.peek().left = cur;
+				stack.push(cur);
+				i++;
+			}
+		}
+		return root.left;
+	}
+
+}

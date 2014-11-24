b5ad9d7f9880e5951658a52f5678e6229ebcce00
opmiss <opmiss@gmail.com>
2014-11-24 15:35:26 -0800

    11242014

--- src/com/leetcode/problems/dp/UniquePaths.java
+++ src/com/leetcode/problems/dp/UniquePaths.java
@@ -11,8 +11,16 @@ public class UniquePaths {
 	 * Note: m and n will be at most 100.
 	 */
     public int uniquePaths(int m, int n) {
-        //TODO: 
-    	return 0;  
+    	int[][] path = new int[m+1][n+1]; 
+        for (int i=0; i<=m; i++) path[i][0] = 0;
+        for (int j=0; j<=n; j++) path[0][j] = 0;
+        for (int i=1; i<=m; i++){
+            for (int j=1; j<=n; j++){
+                if (i==1 && j==1) path[i][j]=1; 
+                else path[i][j] = path[i-1][j] + path[i][j-1]; 
+            }
+        }
+        return path[m][n];  
     }
     
     /*
@@ -30,8 +38,21 @@ public class UniquePaths {
      * The total number of unique paths is 2.
      */
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    	//TODO: 
-    	return 0; 
-        
+    	 int m=obstacleGrid.length; 
+         if (m<1) return 0; 
+         int n = obstacleGrid[0].length; 
+         if (n<1) return 0; 
+         if (obstacleGrid[0][0]==1) return 0; 
+         int[][] p = new int[m+1][n+1];
+         for (int i=0; i<=m; i++) p[i][0]=0; 
+         for (int j=0; j<=n; j++) p[0][j]=0; 
+         for (int i=1; i<=m; i++){
+             for (int j=1; j<=n; j++){
+                 if (i==1&&j==1) p[i][j]=1; 
+                 else if (obstacleGrid[i-1][j-1]==1) p[i][j]=0; 
+                 else p[i][j] = p[i-1][j]+p[i][j-1]; 
+             }
+         }
+         return p[m][n]; 
     }
 }

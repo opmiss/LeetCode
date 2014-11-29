57655eec82883743e5c4b446a9bf22e400d72f3d
opmiss <opmiss@gmail.com>
2014-11-29 12:47:14 -0800

    11292014

--- src/com/leetcode/problems/math/ValidNumber.java
+++ src/com/leetcode/problems/math/ValidNumber.java
@@ -12,8 +12,24 @@ package com.leetcode.problems.math;
  */
 
 public class ValidNumber {
-	public static boolean isNumber(String s){
-		return true; 
-	}
-
+    public boolean isNumber(String s) {
+        s = s.trim(); 
+        int e = s.indexOf("e"); 
+        if (e>0){
+            return isRegularNumber(s.substring(0, e)) && isPowerNumber(s.substring(e+1)); 
+        }
+        else return isRegularNumber(s); 
+      }
+      
+      public boolean isRegularNumber(String s){
+        if (s.length()<1) return false; 
+        String p1 = "[+-]?[0-9]+([.][0-9]*)?"; 
+        String p2 = "[+-]?[0-9]*[.][0-9]+";
+        return s.matches(p1) || s.matches(p2); 
+      }
+      
+      public boolean isPowerNumber(String s){
+          String p = "[+-]?[0-9]+"; 
+          return s.matches(p); 
+      }
 }

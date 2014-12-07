6c369e26abee19abdca1459201bfc8c3cf749ae8
opmiss <opmiss@gmail.com>
2014-12-06 17:16:41 -0800

    12062014

--- src/com/leetcode/problems/linkedlist/ReverseNodesGroup.java
+++ src/com/leetcode/problems/linkedlist/ReverseNodesGroup.java
@@ -20,7 +20,29 @@ public class ReverseNodesGroup {
 			}
 		}
 	public ListNode reverseKGroup(ListNode head, int k) {
-		//TODO
-		return null; 
+		if (k<=1 || head==null) return head; 
+		ListNode dummyhead = new ListNode(0);
+		dummyhead.next = head; 
+		ListNode start = dummyhead; 
+		ListNode end = head; 
+		ListNode cur = head.next;
+		ListNode nextStart, nextCur; 
+		while (head!=null){
+			for (int i=0; i<k; i++) {
+				end = end.next;
+				if (end==null && i<k-1) return dummyhead.next; 
+			}
+			while (cur!=end){
+				nextCur = cur.next; 
+				nextStart = start.next; 
+				start.next = cur; 
+				cur.next = nextStart; 
+				head.next = nextCur; 
+				cur = nextCur; 
+			}
+			start = head; 
+			head = end; 
+		}
+		return dummyhead.next; 
     }
 }

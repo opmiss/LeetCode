e4fb7a5f2ea07d1ee8c7031ce6073a6f24e23f3e
opmiss <opmiss@gmail.com>
2014-12-13 13:04:22 -0800

    12132014

--- src/com/leetcode/problems/string/InterleavingString.java
+++ src/com/leetcode/problems/string/InterleavingString.java
@@ -11,12 +11,34 @@ package com.leetcode.problems.string;
  * When s3 = "aadbbbaccc", return false.
  */
 
-
 public class InterleavingString {
-	 public boolean isInterleave(String s1, String s2, String s3) {
-	     //TODO:; 
-		 return false; 
-	        
-	    }
-
+	public boolean isInterleave(String s1, String s2, String s3) {
+		int l1 = s1.length(), l2 = s2.length();
+		if (l1 + l2 != s3.length())
+			return false;
+		if (l2 == 0) {
+			if (s1.equals(s3))
+				return true;
+			else
+				return false;
+		}
+		boolean[] visit = new boolean[l2 + 1];
+		for (int i = 0; i <= l1; i++) {
+			for (int j = 0; j <= l2; j++) {
+				if (i == 0 && j == 0)
+					visit[j] = true;
+				else if (i == 0) {
+					if (s3.charAt(j - 1) == s2.charAt(j - 1))
+						visit[j] = visit[j - 1];
+				} else if (j > 0) {
+					boolean b1 = (s3.charAt(i + j - 1) == s1.charAt(i - 1))
+							&& visit[j];
+					boolean b2 = (s3.charAt(i + j - 1) == s2.charAt(j - 1))
+							&& visit[j - 1];
+					visit[j] = b1 || b2;
+				}
+			}
+		}
+		return visit[l2];
+	}
 }

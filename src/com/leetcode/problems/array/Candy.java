03b517aae44dbc293252442992e6f60778db6829
opmiss <opmiss@gmail.com>
2014-12-22 22:26:15 -0800

    majority

--- src/com/leetcode/problems/array/Candy.java
+++ src/com/leetcode/problems/array/Candy.java
@@ -1,4 +1,7 @@
 package com.leetcode.problems.array;
+
+import java.util.HashMap;
+import java.util.Map;
 /*
  * There are N children standing in a line. Each child is assigned a rating value.
  * You are giving candies to these children subjected to the following requirements:
@@ -10,6 +13,23 @@ package com.leetcode.problems.array;
 public class Candy {
     public int candy(int[] ratings) {
         return 0; 
+    }
+    public int majorityElement(int[] num) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); 
+        for (int n:num){
+            Integer c = map.get(n);
+            if (c==null) c=0; 
+            c++; 
+            if (c>num.length/2) return n; 
+            map.put(n, c);
+        }
+        return 0; 
+    }
+    
+    public static void main(String[] args){
+    	String a= "asdfsdg"; 
+    	
+    	System.out.println(Integer.parseInt("001")); 
     }
 
 }

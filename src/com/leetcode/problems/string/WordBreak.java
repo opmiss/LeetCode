1af75f8342fc92982ec91903128968c99c162a00
opmiss <opmiss@gmail.com>
2014-12-24 10:48:27 -0800

    word break

--- src/com/leetcode/problems/string/WordBreak.java
+++ src/com/leetcode/problems/string/WordBreak.java
@@ -1,5 +1,7 @@
 package com.leetcode.problems.string;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -13,8 +15,47 @@ public class WordBreak {
 	 * dict = ["cat", "cats", "and", "sand", "dog"].
 	 * A solution is ["cats and dog", "cat sand dog"].
 	 */
-	 public List<String> wordBreak(String s, Set<String> dict) {
-		 return null; 
+	public List<String> wordBreak(String s, Set<String> dict) {
+		 int[] r = getLengthRange(dict);
+		 return helper(s, dict, r[0], r[1]); 
+	 }
+	 
+	 public List<String> helper(String s, Set<String> dict, int minL, int maxL){
+		 List<String> ret = new ArrayList<String>();
+		 if (s==null || s.length() < minL) return ret; 
+		 int sl= s.length(); 
+		 maxL = (maxL<sl)?maxL:sl; 
+		 
+		 boolean stop = true; 
+		 for (int i=minL; i<=maxL; i++) if (dict.contains(s.substring(s.length()-i))) {stop=false; break;} 
+		 if (stop) return ret; 
+		 
+		 String head; 
+		 for (int i=minL; i<=maxL; i++){
+			 head = s.substring(0, i); 
+			 if (dict.contains(head)){
+			     if (i==sl) {
+			    	 ret.add(head); 
+			    	 return ret; 
+			     }
+			     List<String> rest = helper(s.substring(i), dict, minL, maxL); 
+			     for (String p:rest){ 
+			    	 ret.add(head+" "+p); 
+			     }
+			 }
+		 }
+		 return ret; 
+	 }
+	
+	 int[] getLengthRange(Set<String> dict){
+		 int[] range=new int[2]; 
+		 range[0] = Integer.MAX_VALUE; 
+		 for (String s:dict){
+		     int sl = s.length();
+			 range[0] = (range[0]<sl)? range[0]:sl; 
+			 range[1] = (range[1]>sl)? range[1]:sl; 
+		 }
+		 return range; 
 	 }
 	 
 	 /*
@@ -25,7 +66,38 @@ public class WordBreak {
 	  * Return true because "leetcode" can be segmented as "leet code".
 	  */
 	  public boolean canBreak(String s, Set<String> dict) {
-		  return false; 
+		  boolean[][] canBreak = new boolean[s.length()][s.length()]; 
+		  for (int k=0; k<s.length(); k++){
+			  for (int i=0; i<s.length()-k; i++){
+				  canBreak[i][i+k] = dict.contains(s.substring(i, i+k+1)); 
+				  int j=0; 
+				  while (!canBreak[i][i+k]&&j<k){
+					  canBreak[i][i+k] = canBreak[i][i+j] && canBreak[i+j+1][i+k];
+					  j++; 
+				  }
+			  }
+		  }
+		  return canBreak[0][s.length()-1]; 
+	  }
+	  
+	  public static void main(String[] args){
+		  Set<String> dict = new HashSet<String>(); 
+		  dict.add("hello"); 
+		  dict.add("world"); 
+		  String s = "helloworld"; 
+		  WordBreak wb=new WordBreak(); 
+		  System.out.println(wb.canBreak(s, dict)); 
+		  
+		  dict.add("cat"); 
+		  dict.add("cats"); 
+		  dict.add("and"); 
+		  dict.add("sand"); 
+		  dict.add("dog"); 
+		  
+		  s = "catsanddog"; 
+		  
+		  List<String> list=wb.wordBreak(s, dict);
+		  for (String l:list) System.out.println(l); 
 	  }
 
 }

91fa7d9b6bbe5b915e4e9287e20d745ac73e31da
opmiss <opmiss@gmail.com>
2015-01-04 20:29:41 -0800

    max path sum

--- src/com/leetcode/problems/tree/PathSum.java
+++ src/com/leetcode/problems/tree/PathSum.java
@@ -9,11 +9,25 @@ package com.leetcode.problems.tree;
       / \
      2   3
      
-     Return 6.
+   Return 6.
  */
 
 public class PathSum {
     public int maxPathSum(TreeNode root) {
-        return 0;
+    	maxSum=Integer.MIN_VALUE; 
+        sideSum(root); 
+        return maxSum; 
+    } 
+    int maxSum=Integer.MIN_VALUE; 
+    public int sideSum(TreeNode node){
+    	if (node==null) return 0; 
+    	int leftSum = sideSum(node.left); 
+    	int rightSum = sideSum(node.right); 
+    	int ret = (leftSum<rightSum)?rightSum:leftSum;
+    	ret = (ret<0)?node.val:(ret+node.val); 
+    	int sum = leftSum+rightSum+node.val; 
+     	maxSum = (maxSum<ret)?ret:maxSum; 
+     	maxSum = (maxSum<sum)?sum:maxSum; 
+    	return ret; 
     }
 }

cc25a1fa8fb48c774875e959ae581696542b2140
opmiss <opmiss@gmail.com>
2015-01-06 17:28:24 -0800

    copy list

--- src/com/leetcode/problems/linkedlist/CopyList.java
+++ src/com/leetcode/problems/linkedlist/CopyList.java
@@ -1,5 +1,8 @@
 package com.leetcode.problems.linkedlist;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
  * Return a deep copy of the list.
@@ -11,7 +14,38 @@ public class CopyList {
 		RandomListNode next, random;
 		RandomListNode(int x) { this.label = x; }
 	};
+	
+	Map<RandomListNode, RandomListNode> map; 
+	
     public RandomListNode copyRandomList(RandomListNode head) {
-        return null; 
+    	map = new HashMap<RandomListNode, RandomListNode>(); 
+    	RandomListNode chead = null; 
+    	chead=copy(head, chead); 
+        return chead; 
+    }
+    public RandomListNode copy(RandomListNode from, RandomListNode to){
+    	if (from==null) return null; 
+    	RandomListNode node = map.get(from);
+    	if (node==null) {
+    		if (to==null) to = new RandomListNode(from.label); 
+    		map.put(from, to); 
+    	}
+    	else {
+    		to = node; 
+    		return to; 
+    	}
+    	to.next = copy(from.next, to.next);
+    	to.random = copy(from.random, to.random); 
+    	return to; 
+    }
+    public void test(){
+    	RandomListNode head = new RandomListNode(-1); 
+    	RandomListNode chead = copyRandomList(head);
+    	System.out.println(chead.label); 
+    }
+    
+    public static void main(String[] args){
+    	CopyList cl = new CopyList(); 
+    	cl.test(); 
     }
 }

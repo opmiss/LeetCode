48ad8739e2ec0905c7c071180409bb245e57d287
opmiss <opmiss@gmail.com>
2015-01-25 13:45:46 -0800

    kip search for string matching

---
+++ src/com/leetcode/problems/string/KMPSearch.java
@@ -0,0 +1,54 @@
+package com.leetcode.problems.string;
+
+public class KMPSearch {
+	
+	public int[] computeTable(String W){
+		int[] T = new int[W.length()]; 
+		int pos = 2, cnd=0; 
+		T[0]=-1; T[1]=0; 
+		while (pos <W.length()){
+			if (W.charAt(pos-1)==W.charAt(cnd)){
+				T[pos]=++cnd; 
+				pos++;
+			}
+			else if (cnd>0){
+				cnd=T[cnd]; 
+			}
+			else T[pos++]=0; 
+		}
+		return T; 
+	}
+	public int strStr(String S, String W){
+		int m=0, i=0; 
+		int[] T=computeTable(W); 
+		while (m+i<S.length()){
+			if (W.charAt(i) == S.charAt(i+m)){
+				if (i==W.length()-1) return m; 
+				i++; 
+			}
+			else {
+				if (T[i]>-1) {
+					m=m+i-T[i]; 
+					i=T[i]; 
+				}
+				else {
+					i=0; 
+					m=m+1; 
+				}
+			}
+		}
+		return -1; 
+	}
+	
+	public void test(){
+		String haystack = "participating parachute";
+		String needle = "pat"; 
+		System.out.println(strStr(haystack, needle)); 
+		
+	}
+	
+	public static void main(String[] args){
+		KMPSearch kmp = new KMPSearch(); 
+		kmp.test(); 
+	}
+}

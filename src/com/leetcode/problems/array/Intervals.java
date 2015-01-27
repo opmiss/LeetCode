a4e1b81f38045bd943f989bb06dc9642efa6ead8
opmiss <opmiss@gmail.com>
2015-01-26 17:35:59 -0800

    intervals

--- src/com/leetcode/problems/array/Intervals.java
+++ src/com/leetcode/problems/array/Intervals.java
@@ -1,7 +1,7 @@
 package com.leetcode.problems.array;
 
 import java.util.ArrayList;
-import java.util.Vector;
+import java.util.List;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -21,38 +21,22 @@ public class Intervals {
 			end = e;
 		}
 	}
-
-	public class IntervalComparator implements Comparator<Interval> {
-		@Override
-		public int compare(Interval i1, Interval i2) {
-			if (i1.start > i2.start)
-				return 1;
-			else if (i1.start < i2.start)
-				return -1;
-			else {
-				if (i1.end > i2.end)
-					return 1;
-				else if (i1.end < i2.end)
-					return -1;
-				else
-					return 0;
-			}
-		}
-	}
-	
 	/*
 	 * Given a collection of intervals, merge all overlapping intervals.
 	 * For example,
 	 * Given [1,3],[2,6],[8,10],[15,18],
 	 * return [1,6],[8,10],[15,18].
-	 * */
-
-	public Vector<Interval> merge(Vector<Interval> intervals) {
+	 */
+	public List<Interval> merge(List<Interval> intervals) {
 		if (intervals.size() < 2)
 			return intervals;
-		Vector<Interval> ret = new Vector<Interval>();
-		IntervalComparator c = new IntervalComparator();
-		Collections.sort(intervals, c);
+		List<Interval> ret = new ArrayList<Interval>();
+		Collections.sort(intervals, new Comparator<Interval>(){
+			@Override
+			public int compare(Interval o1, Interval o2) {
+				return o1.start-o2.start;
+			}
+			});
 		Interval cur = intervals.get(0);
 		for (int i = 1; i < intervals.size(); i++) {
 			Interval val = intervals.get(i);
@@ -62,14 +46,13 @@ public class Intervals {
 			} else if (cur.start > val.end) {
 				ret.add(val);
 			} else {
-				cur = new Interval(Math.min(cur.start, val.start), Math.max(
-						cur.end, val.end));
+				cur.start = (cur.start<val.start)?cur.start:val.start; 
+				cur.end = (cur.end>val.end)?cur.end:val.end;
 			}
 		}
 		ret.add(cur);
 		return ret;
 	}
-	
 	/*
 	 * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
 	 * You may assume that the intervals were initially sorted according to their start times.
@@ -81,14 +64,14 @@ public class Intervals {
 	 * */
 	
 	public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
-        ArrayList<Interval> ret = new ArrayList<Interval>(); 
+		ArrayList<Interval> ret = new ArrayList<Interval>(); 
         if (intervals.size()==0) { ret.add(newInterval); return ret; }
         for (Interval i: intervals){
             if (i.end < newInterval.start) ret.add(i);
             else if (i.start > newInterval.end) {ret.add(newInterval); newInterval=i; }
             else {
-                Interval m = new Interval(Math.min(i.start, newInterval.start), Math.max(i.end, newInterval.end));
-                newInterval = m; 
+                newInterval.start = (i.start<newInterval.start)?i.start:newInterval.start; 
+                newInterval.end = (i.end>newInterval.end)?i.end:newInterval.end;
             }
         }
         ret.add(newInterval); 

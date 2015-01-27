82db45c48a6066f1cfa51c355967f043704a7705
opmiss <opmiss@gmail.com>
2015-01-26 17:35:08 -0800

    copy random list without hash map

--- src/com/leetcode/problems/linkedlist/CopyList.java
+++ src/com/leetcode/problems/linkedlist/CopyList.java
@@ -1,8 +1,5 @@
 package com.leetcode.problems.linkedlist;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /*
  * A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
  * Return a deep copy of the list.

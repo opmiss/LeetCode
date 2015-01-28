aae37557f988a8f5a9f1154744f2df1b8154a116
opmiss <opmiss@gmail.com>
2015-01-27 17:03:30 -0800

    buy sell stock

--- src/com/leetcode/problems/dp/BuySellStock.java
+++ src/com/leetcode/problems/dp/BuySellStock.java
@@ -9,31 +9,52 @@ package com.leetcode.problems.dp;
  * */
 
 public class BuySellStock {
-	 public int maxProfit(int[] prices) {
-	        int maxProfit =0; 
-	        if (prices.length<2) return 0;
-	        int[] historyProfit = new int[prices.length]; 
-	        historyProfit[0] = 0; 
-	        int[] futureProfit = new int[prices.length]; 
-	        futureProfit[prices.length-1] = 0; 
-	        int valley = prices[0]; 
-	        int peak = prices[prices.length-1]; 
-	        int i=1;
-	        while (i<prices.length){
-	            valley = Math.min(valley, prices[i]);
-	            historyProfit[i] = Math.max(historyProfit[i-1], prices[i]-valley); 
-	            i++; 
-	        }
-	        int profit =0; 
-	        i = prices.length-2; 
-	        while (i>=0){
-	            peak = Math.max(peak, prices[i]); 
-	            futureProfit[i] = Math.max(futureProfit[i+1], peak - prices[i]);
-	            profit = futureProfit[i] + historyProfit[i]; 
-	            if (profit > maxProfit) maxProfit = profit; 
-	            i--; 
-	        }
-	        return maxProfit; 
-	    }
+	/*
+	 * Say you have an array for which the ith element is the price of a given stock on day i.
+	 * Design an algorithm to find the maximum profit. You may complete at most two transactions.
+	 * Note:
+	 * You may not engage in multiple transactions at the same time 
+	 * (ie, you must sell the stock before you buy again).
+	 * */
+	 
+	public int max(int a, int b){
+		return (a>b)?a:b; 
+	}
+	 public int maxProfit2(int[] prices) {
+		 int buy1 = Integer.MIN_VALUE, sell1 =0, buy2 = Integer.MIN_VALUE, sell2=0; 
+		 for (int p:prices){
+			 sell2 = max(sell2, p+buy2); 
+			 buy2 = max(buy2, sell1-p);
+			 sell1 = max(sell1, p+buy1);
+			 buy1 = max(buy1, -p); 
+		 }
+		 return sell2; 
+	 }
+	 
+	 /* solution to the problem generalized to N transactions*/ 
+	 public int maxProfitK(int[] prices, int K){
+		 int[] profit = new int[K*2]; 
+		 for (int k=0; k<K; k++) profit[k*2] = Integer.MIN_VALUE; 
+		 for (int p:prices){
+			 for (int k=K-1; k>=0; k--){
+				 int i=k*2; 
+				 profit[i+1] = max(profit[i+1], p+profit[i]); 
+				 if (i==0) profit[i] = max(profit[i], 0-p); 
+				 else profit[i] = max(profit[i], profit[i-1]-p); 
+			 }
+		 }
+		 return profit[K*2-1]; 
+	 }
+	 
+	 public void test(){
+		 int[] A = new int[]{1, 2, 0, 5, 2, 3, 4}; 
+		 System.out.println(maxProfit2(A)); 
+		 System.out.println(maxProfitK(A, 3)); 
+	 }
+	 
+	 public static void main(String[] args){
+		 BuySellStock bss = new BuySellStock(); 
+		 bss.test(); 
+	 }
 
 }

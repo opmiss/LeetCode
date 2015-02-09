b13acdbec0d85973c42954516c5a1d1414c45f49
opmiss <opmiss@gmail.com>
2015-02-08 23:21:10 -0800

    dungeonGame

--- src/com/leetcode/problems/dp/DungeonGame.java
+++ src/com/leetcode/problems/dp/DungeonGame.java
@@ -28,9 +28,7 @@ public class DungeonGame {
 	 public int calculateMinimumHP(int[][] dungeon) {
 		 int n = dungeon.length; 
 		 int m = dungeon[0].length; 
-		 
 		 int[][] hp = new int[n][m]; 
-		 
 		 for (int i = n-1; i>=0; i--){
 			 for (int j= m-1; j>=0; j--){
 				if (i==n-1 && j==m-1){

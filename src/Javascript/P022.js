53bca4d282b781d951d44bdf1b435b53b5733c7a
opmiss <opmiss@gmail.com>
2015-03-08 08:56:38 -0700

    p022

---
+++ src/Javascript/P022.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    if (n===0) return [""];
+    if (n===1) return ["()"];
+    var res = [];
+    for (var i=0; i<n; i++){
+        var inner = generateParenthesis(i);
+        var outter = generateParenthesis(n-i-1);
+        inner.forEach(function(i){
+                      outter.forEach(function(o){
+                                     res.push('('+i+')'+o);
+                                     });
+                      });
+    }
+    return res; 
+};

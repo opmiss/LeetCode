9fd11237bc9b250bb9d165e3969097456084c202
opmiss <opmiss@gmail.com>
2015-05-19 17:20:16 -0700

    add P150-P159

---
+++ src/com/leetcode/algorithms/P152_MaximumProductSubarray.java
@@ -0,0 +1,26 @@
+package com.leetcode.algorithms;
+
+public class P152_MaximumProductSubarray {
+	public int maxProduct(int[] A) {
+        if (A.length==1) return A[0];
+        int maxhere = 0; 
+        int minhere = 0; 
+        int maxprod = A[0];
+        for (int i=0; i<A.length; i++){
+            if (A[i]>0){
+                maxhere = (maxhere>0)?maxhere*A[i]:A[i];
+                minhere = (minhere<0)?minhere*A[i]:0; 
+            }
+            else if (A[i]<0) {
+                int t = maxhere; 
+                maxhere = (minhere<0)?minhere*A[i]:0; 
+                minhere = (t>0)?t*A[i]:A[i]; 
+            }
+            else {
+                maxhere=minhere=0; 
+            }
+            maxprod = (maxprod<maxhere)?maxhere:maxprod;
+        }
+        return maxprod;
+    }
+}
---
+++ src/com/leetcode/algorithms/P156_BinaryTreeUpsideDown.java
@@ -0,0 +1,5 @@
+package com.leetcode.algorithms;
+
+public class P156_BinaryTreeUpsideDown {
+
+}

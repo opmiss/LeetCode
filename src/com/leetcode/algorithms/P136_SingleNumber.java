de32f42d5e0f6d8e5621bf5527cb66a83da5ce84
opmiss <opmiss@gmail.com>
2015-08-13 17:03:23 -0700

    single number

--- src/com/leetcode/algorithms/P136_SingleNumber.java
+++ src/com/leetcode/algorithms/P136_SingleNumber.java
@@ -1,10 +1,28 @@
 package com.leetcode.algorithms;
 
 public class P136_SingleNumber {
+	
 	public int singleNumber(int[] A) {
         int s = A[0]; 
 		for (int i=1; i<A.length; i++)
 			s=s^A[i]; 
         return s; 
+    }
+	
+	public int singleNumberII(int[] A) {
+        int m1 = A[0]; 
+        int m2 = 0; 
+        int _m2; 
+        for (int i=1; i<A.length; i++){
+            _m2 = m2; 
+            m2 = (m1&A[i])|(_m2&(~A[i]));
+            m1 = (m1&(~A[i]))|((~m1)&(~_m2)&A[i]); 
+        }
+        return m1; 
+    }
+	
+	public int[] singleNumberIII(int[] A) {
+        //TODO 
+		return null; 
     }
 }

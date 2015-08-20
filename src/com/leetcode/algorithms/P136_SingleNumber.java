dd72737ec302c282461a217b00c5630ef28dfe71
opmiss <opmiss@gmail.com>
2015-08-20 11:31:33 -0700

    single number 3

--- src/com/leetcode/algorithms/P136_SingleNumber.java
+++ src/com/leetcode/algorithms/P136_SingleNumber.java
@@ -22,7 +22,25 @@ public class P136_SingleNumber {
     }
 	
 	public int[] singleNumberIII(int[] A) {
-        //TODO 
-		return null; 
+		int or = 0; 
+        for(int i=0; i<A.length; i++){
+        	or = or^A[i]; 
+        }
+        or = or&(-or);
+        int[] res = new int[2]; 
+        for (int i=0; i<A.length; i++){
+        	if ((A[i]&or)==0){
+        		res[0]^=A[i]; 
+        	}else {
+        		res[1]^=A[i]; 
+        	}
+        }
+		return res; 
     }
+	
+	public static void main(String[] args){
+		P136_SingleNumber p = new P136_SingleNumber(); 
+		int[] singles = p.singleNumberIII(new int[]{1, 2, 3, 1, 2, 3, 8, 6}); 
+		System.out.println(singles[0]+", "+singles[1]); 
+	}
 }

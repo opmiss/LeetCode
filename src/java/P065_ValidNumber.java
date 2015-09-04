4d88595c367cebfadcc0d2b75102750a328f27d8
opmiss <opmiss@gmail.com>
2015-09-04 12:41:29 -0700

    add math problems

--- src/java/P065_ValidNumber.java
+++ src/java/P065_ValidNumber.java
@@ -41,4 +41,25 @@ public class P065_ValidNumber {
         } 
         return (i==n)&&number; 
     }
+	//another solution
+	public boolean isValidNumber(String s) {
+        s = s.trim(); 
+        int e = s.indexOf("e"); 
+        if (e>0){
+            return isRegularNumber(s.substring(0, e)) && isPowerNumber(s.substring(e+1)); 
+        }
+        else return isRegularNumber(s); 
+      }
+      
+      public boolean isRegularNumber(String s){
+        if (s.length()<1) return false; 
+        String p1 = "[+-]?[0-9]+([.][0-9]*)?"; 
+        String p2 = "[+-]?[0-9]*[.][0-9]+";
+        return s.matches(p1) || s.matches(p2); 
+      }
+      
+      public boolean isPowerNumber(String s){
+          String p = "[+-]?[0-9]+"; 
+          return s.matches(p); 
+      }
 }

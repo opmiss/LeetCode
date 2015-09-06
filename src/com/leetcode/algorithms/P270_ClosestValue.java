08cc064345de4bedad93896bd6ad80b9e78145b9
opmiss <opmiss@gmail.com>
2015-09-06 10:27:29 -0700

    closest value

--- src/com/leetcode/algorithms/P270_ClosestValue.java
+++ src/com/leetcode/algorithms/P270_ClosestValue.java
@@ -4,8 +4,24 @@ import java.util.*;
 
 public class P270_ClosestValue {
 	public int closestValue(TreeNode root, double target) {
-		//TODO
-		return 0; 
+		double closest = Integer.MAX_VALUE;
+	    int value = 0;
+	    TreeNode current = root;
+	    while (current != null) {
+	        if (closest > Math.abs(current.val-target)) {
+	            closest = Math.abs(current.val-target);
+	            value = current.val;
+	        }
+
+	        if (current.val < target) {
+	            current = current.right;
+	        } else if (current.val > target) {
+	            current = current.left;
+	        } else {
+	            break;
+	        }
+	    }
+	    return value; 
 	}
 	public List<Integer> closestKValues(TreeNode root, double target, int k) {
 		//TODO 

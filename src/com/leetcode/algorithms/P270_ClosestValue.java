744f88199429f4dd3c4008bb483478b0638a76a5
opmiss <opmiss@gmail.com>
2015-09-07 10:13:47 -0700

    closest k value

--- src/com/leetcode/algorithms/P270_ClosestValue.java
+++ src/com/leetcode/algorithms/P270_ClosestValue.java
@@ -1,5 +1,15 @@
 package com.leetcode.algorithms;
+import com.leetcode.problems.tree.*;
+import java.util.*;
 
 public class P270_ClosestValue {
+	public int closestValue(TreeNode root, double target) {
+		//TODO
+		return 0; 
+	}
+	public List<Integer> closestKValues(TreeNode root, double target, int k) {
+		//TODO 
+		return null; 
+	}
 
 }

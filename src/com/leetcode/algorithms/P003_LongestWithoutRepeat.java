ef884009e57bda96d2d251f6f7a23c73619c7734
opmiss <opmiss@gmail.com>
2015-10-03 19:21:51 -0700

    P1-P5

--- src/com/leetcode/algorithms/P003_LongestWithoutRepeat.java
+++ src/com/leetcode/algorithms/P003_LongestWithoutRepeat.java
@@ -1,5 +1,31 @@
 package com.leetcode.algorithms;
+import java.util.*;
 
 public class P003_LongestWithoutRepeat {
+	HashMap<Character, Integer> map; 
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length()==1) return 1; 
+        map = new HashMap<Character, Integer>(); 
+        int start = 0; 
+        int v = 0; 
+        int maxlength = 0; 
+        while (v<s.length()){
+            Integer p = map.get(s.charAt(v)); 
+            if (p==null) {
+                map.put(s.charAt(v), v); 
+            }
+            else {
+                maxlength = Math.max(maxlength, v-start); 
+                for (int i=start; i<=p; i++){
+                    map.remove(s.charAt(i)); 
+                }
+                start = p+1; 
+                map.put(s.charAt(v), v); 
+            }
+            v++; 
+        }
+        maxlength = Math.max(maxlength, v-start); 
+        return maxlength; 
+    }
 
 }

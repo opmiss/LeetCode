f70fd95f43f252711ddf90a49fb9a20e107957bc
opmiss <opmiss@gmail.com>
2015-10-04 19:25:59 -0700

    P6-8

--- src/com/leetcode/algorithms/P007_ReverseInteger.java
+++ src/com/leetcode/algorithms/P007_ReverseInteger.java
@@ -1,5 +1,14 @@
 package com.leetcode.algorithms;
 
 public class P007_ReverseInteger {
+	 public int reverse(int x) {
+	        boolean neg = false; 
+	        if (x < 0) {neg = true; x=-x; }
+	        String xs = Integer.toString(x);
+	        xs = (new StringBuilder(xs)).reverse().toString();
+	        x = Integer.parseInt(xs); 
+	        if (neg) x= -x; 
+	        return x; 
+	    }
 
 }

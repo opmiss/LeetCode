fbda9edf59c9ef9d1ca7dc5cf884033ba37e224e
opmiss <opmiss@gmail.com>
2015-11-11 08:55:22 -0800

    p021

---
+++ src/Javascript/P021.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    var dummy = new ListNode(0);
+    var prev = dummy;
+    while (l1!==null && l2!==null){
+        if (l1.val<l2.val){
+            prev.next = l1;
+            l1 = l1.next;
+        }else{
+            prev.next = l2;
+            l2 = l2.next;
+        }
+        prev = prev.next;
+    }
+    if (l1===null) prev.next = l2;
+    else prev.next = l1;
+    return dummy.next;
+};
